1cdcfa54f66797c553fdac833f3e985f26521a2d
tiyo5151 <s1F102400224@iniad.org>
2024-12-01 23:24:38 +0900

    add font

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,15 @@
-import { UserCircle, Settings } from 'lucide-react'
-import { Button } from '@/components/ui/button'
+import { UserCircle, Settings } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <div className="mr-4 hidden md:flex">
-          <a className="mr-6 flex items-center space-x-2" href="/">
-            <span className="hidden font-bold sm:inline-block">MemoApp</span>
+          <a className="mx-6 flex items-center space-x-2" href="/">
+            <span className="hidden font-fugaz font-bold text-2xl sm:inline-block">
+              The Note
+            </span>
           </a>
         </div>
         <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
@@ -24,6 +26,5 @@ export function Header() {
         </div>
       </div>
     </header>
-  )
+  );
 }
-
