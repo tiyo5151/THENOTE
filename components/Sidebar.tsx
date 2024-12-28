3bfdf64127f658b12375eb740171822dba2ff115
tiyo5151 <s1F102400224@iniad.org>
2024-12-28 23:54:53 +0900

    sidebar css

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 
 const Sidebar: React.FC<NoteDisplayProp> = ({ notes }) => {
   return (
-    <div className="flex w-64 min-w-32 h-full bg-slate-800">
-      <ul>
+    <div className="flex justify-center items-start px-2 py-4 w-64 min-w-32 h-full bg-slate-800">
+      <ul className="flex flex-col justify-start items-center w-full h-full">
         {notes.map((note) => (
-          <li key={note.id}>{note.title}</li>
+          <li
+            key={note.id}
+            className="flex justify-center items-center my-1 bg-white w-full h-16"
+          >
+            {note.title}
+          </li>
         ))}
       </ul>
     </div>
