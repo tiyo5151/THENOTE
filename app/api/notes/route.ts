cfa00218745841c152bf68189d9a9b63b0ce35f3
tiyo5151 <s1F102400224@iniad.org>
2025-01-17 23:48:11 +0900

    refactoring

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,7 +1,19 @@
+import { Note } from '@/app/types/types';
 import { NextResponse } from 'next/server';
 
+async function getNotes() {
+  const res = await fetch('http://localhost:3001/notes', {
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch notes');
+  }
+
+  return res.json();
+}
+
 export async function GET() {
-  const res = await fetch('http://localhost:3001/notes');
-  const notes = await res.json();
+  const notes: Note[] = await getNotes();
   return NextResponse.json(notes);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,46 @@
+'use client';
 import { Note } from '@/app/types/types';
-import NoteClientWrapper from '@/components/NoteClientWrapper';
+import { useEffect, useState } from 'react';
+import Header from '../components/Header';
+import NoteDisplay from '../components/NoteDisplay';
+import Sidebar from '../components/Sidebar';
 
-async function getNotes() {
-  const res = await fetch('http://localhost:3001/notes', {
-    cache: 'no-store',
-  });
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch notes');
-  }
-
-  return res.json();
-}
-
-export default async function Page() {
-  const notes: Note[] = await getNotes();
-  return <NoteClientWrapper notes={notes} />;
+export default function Home() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [selectNote, setSelectNote] = useState<string | null>(null);
+  const selectNoteContent = notes.find((note) => note.id === selectNote);
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await fetch('api/notes');
+        if (!res.ok) {
+          throw new Error('Failed to fetch notes');
+        }
+        const data = await res.json();
+        setNotes(data);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : 'Failed to fetch notes');
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+  return (
+    <div className="flex flex-col h-screen mx-auto">
+      <Header />
+      <div className="w-full flex flex-row overflow-hidden h-full">
+        <Sidebar
+          notes={notes}
+          selectNote={selectNote}
+          setSelectNote={setSelectNote}
+        />
+        <NoteDisplay selectNoteContent={selectNoteContent} />
+      </div>
+    </div>
+  );
 }
